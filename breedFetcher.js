c908357f85059b6c9b916dd245d17c70d6a49173
Cedric.gauthier <cidgaut@gmail.com>
2024-01-18 17:56:34 -0500

    api search now takes command line argument to search breed. console.log outputs to see body before and after turning it into an object. only retrieves data when command line provides the argument, otherwise type of data changes but it still shows []. added console.log results to file to show changes

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,29 @@
 const request = require('request');
 
 const apiUrl = "https://api.thecatapi.com/v1/breeds/search";
-const breedToSearch = "sib"; // breed search both sib and siberian work for some reason sib is the ID and name is siberian 
+const breedToSearch = process.argv[2]; // breed search is now enterred by the command line.
 
 const url = `${apiUrl}?q=${breedToSearch}`; // ?q= this means (? beginns query, q query, =assigned to cat breedtosearch)
 
-request(url, (/*error, response, */body) => { // searching only for body
-  //console.log('error:', error); // Print the error if one occurred
-  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //console.log('body:', body); // Print the HTML for the Google homepage.
+request(url, (error, response, body) => { // 
+  console.log('body:', body);// Print the HTML for the Google homepage. 
+  console.log('type of body:', typeof body)                             
+  console.log('error:', error); // Print the error if one occurred      
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   
-  const data = JSON.parse(body);
-  console.log(data);
-  console.log(typeof data);
+  
+  const data = JSON.parse(body); //body transformmed by json is stored in data
+  console.log('data:', data); //print 
+  console.log('type of data:', typeof data);
 });
+
+
+/* 
+console.logs
+body: []
+type of body: string
+error: null
+statusCode: 200
+data without command line arg [], data with command line arg sib [{ object filled with all keys and values of sibe}]
+type of data object
+*/
