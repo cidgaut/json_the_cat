1bce9f51045b7b59681ef3fe0836139da2f25459
Cedric.gauthier <cidgaut@gmail.com>
2024-01-18 20:21:18 -0500

    added higher order function with an anonymous arrow function. commented out console.logs that are no longer useful. Function compairs with .some to see if either the ID or name of a breed matches the input from user. User can only provide exact math even though q in search allows for partial name search. Just need to add server error message and done.

--- breedFetcher.js
+++ breedFetcher.js
@@ -6,20 +6,28 @@ const breedToSearch = process.argv[2]; // breed search is now enterred by the co
 const url = `${apiUrl}?q=${breedToSearch}`; // ?q= this means (? beginns query, q query, = assigned to cat breedtosearch)
 
 request(url, (error, response, body) => { // 
-  console.log('body:', body);// Print the HTML for the Google homepage. 
-  console.log('type of body:', typeof body)                             
+  //console.log('body:', body);// Print the HTML for the Google homepage. 
+  ///console.log('type of body:', typeof body)                             
   console.log('error:', error); // Print the error if one occurred      
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   
   
   const data = JSON.parse(body); //body transformmed by json is stored in data
   console.log('data:', data); //print 
-  console.log('type of data:', typeof data);
+  //console.log('type of data:', typeof data);
 
   const searchTerm = breedToSearch.toLowerCase(); // Convert the arg input to lowercase for case-insensitive comparison
+  console.log('searchterm',searchTerm);
+  // Check if arg matches any id or name in the data array. Doing both id and name to lowercase, but name seems to be the only one with upercase
+  const breedFound = data.some(breed => breed.id.toLowerCase() === searchTerm || breed.name.toLowerCase() === searchTerm);
 
-  // Check if arg matches any id or name in the data array
-  
+  if (breedFound) {
+    const description = data[0].description;
+    console.log('Description:', description);
+  } else {
+    console.log('Breed not found');
+  }
+
 });
 
 
