5047d5deb6567bce0966d613ee19d8d7dd2101aa
Cedric.gauthier <cidgaut@gmail.com>
2024-01-18 20:28:09 -0500

    added a conditional to error, which still print error but only if error occurs. also ends the program.

--- breedFetcher.js
+++ breedFetcher.js
@@ -5,10 +5,14 @@ const breedToSearch = process.argv[2]; // breed search is now enterred by the co
 
 const url = `${apiUrl}?q=${breedToSearch}`; // ?q= this means (? beginns query, q query, = assigned to cat breedtosearch)
 
-request(url, (error, response, body) => { // 
+request(url, (error, response, body) => {
   //console.log('body:', body);// Print the HTML for the Google homepage. 
   ///console.log('type of body:', typeof body)                             
-  console.log('error:', error); // Print the error if one occurred      
+  if (error) {
+    console.log('error:', error); // Print the error if one occurred      
+    return;
+  }
+
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   
   
